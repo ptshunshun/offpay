8a195110693d80028ba056ae0b10376b6693e127
nkshn87 <72290373+nkshn87@users.noreply.github.com>
2021-02-20 23:27:11 +0900

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const pool = require('../middlewares/database.js');
-
-router.get('/', (req, res, next) => {
-  res.redirect('/history');
-});
-
-module.exports = router;
